Fix logprobs syntax error and judge prompt escape character (#137)

uqlm/utils/response_generator.py
@@ -187,7 +187,7 @@ class ResponseGenerator:
                         logprobs[i] = result.generations[0][i].generation_info["logprobs_result"]
 
                     elif "logprobs" in result.generations[0][i].generation_info:
-                        if "content" in "logprobs" in result.generations[0][i].generation_info["logprobs"]:
+                        if "content" in result.generations[0][i].generation_info["logprobs"]:
                             logprobs[i] = result.generations[0][i].generation_info["logprobs"]["content"]
                     else:
                         warnings.warn("Model did not provide logprobs in API response. White-box scores for this response may be set to np.nan.")